1185feb8e1cd5b5504dd6e97cf6328f9a321c878
Rafi Budiman <86762683+Gembut@users.noreply.github.com>
2025-10-30 08:39:11 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@
 
 # pip install -r requirements.txt
 # pip install -r requirements.txt --upgrade --ignore-installed --no-deps
-uvicorn MAIN.main:app --reload
+#uvicorn MAIN.main:app --reload
+uvicorn MAIN.main:app --reload --host 0.0.0.0 --port 8010
