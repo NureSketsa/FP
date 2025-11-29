5c4c1e88e503adb7470c1fa5a08e143263d11854
Rafi Budiman <m.rafi.budiman@gmail.com>
2025-11-29 20:54:07 +0700

    supabase self host

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # run dari FP
 # minimum python 3.10
 
-#source venv/bin/activate
-uvicorn MAIN.main:app --reload
-#uvicorn MAIN.main:app --reload --host 0.0.0.0 --port 8000
-#gunicorn MAIN.main:app -k uvicorn.workers.UvicornWorker -w 4 -t 600 -b 0.0.0.0:8000
+source venv/bin/activate
+#uvicorn MAIN.main:app --reload
+uvicorn MAIN.main:app --reload --host 0.0.0.0 --port 8010
+#gunicorn MAIN.main:app -k uvicorn.workers.UvicornWorker -w 4 -t 900 -b 0.0.0.0:8010
