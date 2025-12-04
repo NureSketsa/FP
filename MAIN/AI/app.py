77cbf55fab9e8c7aff91c501f1523ad67bb8a3dc
Rafi Budiman <m.rafi.budiman@gmail.com>
2025-12-04 13:08:28 +0700

    route fix index, sedikit app

--- MAIN/AI/app.py
+++ MAIN/AI/app.py
@@ -87,7 +87,8 @@ def _move_video_to_storage(temp_video_path: str, final_name: str | None = None) 
     shutil.move(str(temp_video), target_path)
 
     # URL publik relative yang akan dilayani oleh FastAPI:
-    public_url = f"/videos/{target_path.name}"
+    # public_url = f"/videos/{target_path.name}"
+    public_url = f"/learnvid-ai/videos/{target_path.name}"
     return str(target_path), public_url
 
 
